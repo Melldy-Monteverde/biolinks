59f12c304da4c8c2975cc1747b2e792d8478eaed
Lex Monteverde <monteverdelex@gmail.com>
2022-11-12 17:56:13 -0300

    change at Swal

--- js/app.js
+++ js/app.js
@@ -2,14 +2,7 @@ function portfolioError() {
   let btnPorfotlio = document.getElementById("btn-portfolio");
 
   btnPorfotlio.addEventListener("click", () => {
-    Swal.fire({
-      title: "Hi!",
-      text: "Sorry, we still working here!",
-      imageUrl: "../assets/img/devWorking.jpg",
-      imageWidth: 400,
-      imageHeight: 200,
-      imageAlt: "dev working",
-    });
+    Swal.fire("Hi!", "Sorry, we still working here!", "info");
   });
 }
 
