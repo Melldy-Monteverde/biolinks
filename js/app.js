8fb05ec2d709b5231d267467cf8af81f62e35706
Lex Monteverde <monteverdelex@gmail.com>
2022-12-26 22:37:04 -0300

    add formulario

--- js/app.js
+++ js/app.js
@@ -2,8 +2,43 @@ function portfolioError() {
   let btnPorfotlio = document.getElementById("btn-portfolio");
 
   btnPorfotlio.addEventListener("click", () => {
-    Swal.fire("Hi!", "Sorry, we still working here!", "info");
+    Swal.fire("Hola!", "Disculpa, sigo trabjando en esta sección!", "info");
   });
 }
 
 portfolioError();
+
+const sent = () => {
+  Swal.fire("Enviado", "Tu consulta fue enviada con exito", "succes");
+};
+
+(() => {
+  "use strict";
+
+  const forms = document.querySelectorAll(".needs-validation");
+  const formID = document.getElementById("form-id");
+
+  formID.setAttribute(
+    "action",
+    "https://formsubmit.co/b5e83f79958bd859bd2f89880fde2ac2"
+  );
+
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+          form.classList.add("was-validated");
+        } else {
+          sent();
+          setTimeout(() => {
+            form.classList.add("was-validated");
+          }, 3000);
+        }
+      },
+      false
+    );
+  });
+})();
